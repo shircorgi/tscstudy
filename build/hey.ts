08f181ce4245dc6de4a66f010a2572f7c43e1e7c
hanul <gusrud21234@gmail.com>
2024-08-12 22:43:09 +0900

    tsc-study 1

---
+++ build/hey.ts
@@ -0,0 +1,7 @@
+"use strict";
+let myname = "egoing";
+// number: 숫자 타입으로, 정수와 실수를 포함합니다.
+// string: 문자열 타입입니다.
+// boolean: 참(true)과 거짓(false)을 나타내는 불리언 타입입니다.
+// null: 값이 없음을 나타내는 타입입니다.
+// undefined: 값이 할당되지 않은 변수의 기본값인 타입입니다.
